38df91a51bc2d1edf1016c52067372c4c7ba0018
ChenxiLi <ChenxiLi@DESKTOP-11UI8I5>
2020-07-16 22:16:29 +0800

    创建了两个业务包，以及相关的代码，测试了版本回退

---
+++ src/main/java/com/zzu/dao/IAirdromeDao.java
@@ -0,0 +1,19 @@
+package com.zzu.dao;
+
+import com.zzu.entity.Airdrome;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface IAirdromeDao {
+    Airdrome getAirdromeById(@Param("id") int id);
+    List<Airdrome> getAllAirdromes();
+    List<Airdrome> getSomeAirdrome(@Param("param")String param);//模糊查询
+    List<Airdrome> getAirByLocation(@Param("location") String location);
+    int insert(@Param("airdrome") Airdrome airdrome);
+    int deleteById(@Param("id")int id);
+    int update(@Param("airdrome")Airdrome airdrome);
+    int updateSelective(@Param("airdrome")Airdrome airdrome);
+}
